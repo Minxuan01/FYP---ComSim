85513ab25f4a3d528d2c9e8a2aa9d133543fc7a1
Minxuan01 <ohmxoff@gmail.com>
2025-09-25 15:30:57 +0800

    added tailwindcss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// @import "tailwindcss"; to use Tailwind CSS with Vite
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',  // Proxy API requests to the backend server, replace with backend server port if different
